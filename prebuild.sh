3d34f354c27d192d951766ae2f3bf62b9d6af180
galaxyofo <xray.rover@gmail.com>
2016-11-16 11:40:49 +0800

    add prebulid.sh

---
+++ prebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cur=$(pwd)
+
+if [ "$1"x = "linux"x ]
+then
+	mkdir linux
+	path=$(pwd)/linux
+	host=
+else 
+	mkdir rt5350
+	path=$(pwd)/rt5350
+	host="--host=mipsel-linux"
+fi
+echo $host $path
+./configure $host --prefix=$path --disable-examples
